e9b803b0d751511412f3089f497a2e0b613c96ed
Lance Warshawsky <lance.warshawsky@gmail.com>
2023-05-15 10:45:03 +0300

    version 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 window.onload = function () {
+    let countryData;
+    const yourCountry = document.createElement("h2");
+    document.body.insertBefore(yourCountry, document.getElementById("select"));
+
     const getCountry = () => {
         fetch("https://api.country.is")
             .then(response => response.json())
             .then(data => {
-                const countryData = data.country;
-                console.log(countryData);
-                const yourCountry = document.createElement("h2");
+                countryData = data.country;
                 yourCountry.textContent = 'Your country is ' + countryData;
-                document.body.appendChild(yourCountry);
-
-                /*fetch(`http://api.mediastack.com/v1/news?access_key=0317afbadd7773a166518d6c1e842e47&countries=${countryData}`)
+                console.log(countryData);
+                
+                fetch(`http://api.mediastack.com/v1/news?access_key=0317afbadd7773a166518d6c1e842e47&countries=${countryData}`)
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
@@ -17,7 +19,22 @@ window.onload = function () {
                     .catch(error => {
                         console.error(error);
                     });
-                    */
+                
+                const selectElement = document.getElementById("select");
+                selectElement.addEventListener("change", (event) => {
+                    countryData = event.target.value;
+                    yourCountry.textContent = 'Your country is ' + countryData;
+                    console.log(countryData);
+
+                    fetch(`http://api.mediastack.com/v1/news?access_key=0317afbadd7773a166518d6c1e842e47&countries=${countryData}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                });
             })
             .catch(err => {
                 console.error(err.message);
